2d2f76390f418ff618d4db6a1308d3ca7b671ec8
Joe DeStefano <jld864@gmail.com>
2023-08-28 10:07:57 -0700

    feat(artifacts): Add support for trigger artifacts (#430)
    feat(artifacts): Add support for trigger artifacts
    
    * fix: Fix yarn lockfile
    
    * fix: Try quoting $GITHUB_OUTPUT in diff step
    
    * fix: Switch to changed-files action
    
    * fix: Forgot to update the condition in the next step
    
    * fix: Fixing changed-files expression
    
    * fix: trying different tact for detecting changes
    
    * chore: Build Source
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -14,11 +14,33 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+const githubContext = {
+  repo: {
+    owner: 'Org',
+    repo: 'actions-test-trigger'
+  },
+  sha: 'long-sha'
+}
+const mockedGetCommit = jest.fn()
+const mockOctokit = {
+  rest: {
+    repos: {
+      getCommit: mockedGetCommit
+    }
+  }
+}
+
 import { SNSClient, PublishCommand } from '@aws-sdk/client-sns'
 import { run } from '../src/main'
 
 // Capture environment variables before running tests
 const cleanEnv = process.env
+jest.mock('@actions/github', () => {
+  return {
+    context: githubContext,
+    getOctokit: () => mockOctokit
+  }
+})
 
 jest.mock('@aws-sdk/client-sns')
 const mockedSend = jest.fn().mockReturnValue({ MessageId: '1' })
@@ -32,6 +54,7 @@ describe('Publish', () => {
     process.env.GITHUB_REPOSITORY = 'Org/actions-test-trigger'
     process.env.GITHUB_SHA = 'long-sha'
     process.env.GITHUB_REF = 'main'
+    process.env.GITHUB_API_URL = 'https://api.github.com'
     SNSClient.prototype.send = mockedSend
   })
 
@@ -41,7 +64,7 @@ describe('Publish', () => {
 
     const input = {
       Message:
-        '{"repository":"Org/actions-test-trigger","commit":"long-sha","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":""}',
+        '{"repository":"Org/actions-test-trigger","commit":"long-sha","githubApiUrl":"https://api.github.com","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":"","modifiedFiles":[]}',
       TopicArn: 'arn:aws:sns:us-west-2:123456789123:spinnaker-github-actions'
     }
 
@@ -52,6 +75,7 @@ describe('Publish', () => {
     expect(SNSClient).toBeCalledWith({ region })
     expect(PublishCommand).toBeCalledWith(input)
     expect(mockedSend).toBeCalledTimes(1)
+    expect(mockedGetCommit).not.toHaveBeenCalled()
   })
 
   test('No REF passed in', async () => {
@@ -61,7 +85,7 @@ describe('Publish', () => {
 
     const input = {
       Message:
-        '{"repository":"Org/actions-test-trigger","commit":"long-sha","ref":"","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":""}',
+        '{"repository":"Org/actions-test-trigger","commit":"long-sha","githubApiUrl":"https://api.github.com","ref":"","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":"","modifiedFiles":[]}',
       TopicArn: 'arn:aws:sns:us-west-2:123456789123:spinnaker-github-actions'
     }
 
@@ -72,6 +96,7 @@ describe('Publish', () => {
     expect(SNSClient).toBeCalledWith({ region })
     expect(PublishCommand).toBeCalledWith(input)
     expect(mockedSend).toBeCalledTimes(1)
+    expect(mockedGetCommit).not.toHaveBeenCalled()
   })
   test('With Parameters and Message Attributes', async () => {
     // Arrange
@@ -81,7 +106,7 @@ describe('Publish', () => {
 
     const input = {
       Message:
-        '{"repository":"Org/actions-test-trigger","commit":"long-sha","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{"parameter1":"value1","parameter2":"value2"},"messageAttributes":"my-attribute"}',
+        '{"repository":"Org/actions-test-trigger","commit":"long-sha","githubApiUrl":"https://api.github.com","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{"parameter1":"value1","parameter2":"value2"},"messageAttributes":"my-attribute","modifiedFiles":[]}',
       TopicArn: 'arn:aws:sns:us-west-2:123456789123:spinnaker-github-actions'
     }
 
@@ -92,6 +117,105 @@ describe('Publish', () => {
     expect(SNSClient).toBeCalledWith({ region })
     expect(PublishCommand).toBeCalledWith(input)
     expect(mockedSend).toBeCalledTimes(1)
+    expect(mockedGetCommit).not.toHaveBeenCalled()
+  })
+
+  describe('when github_token is present', () => {
+    beforeEach(() => {
+      process.env.INPUT_GITHUB_TOKEN = 'token'
+    })
+
+    test('when commit response has no files it returns an empty list for modifiedFiles', async () => {
+      // Arrange
+      const region = 'us-west-2'
+
+      const input = {
+        Message:
+          '{"repository":"Org/actions-test-trigger","commit":"long-sha","githubApiUrl":"https://api.github.com","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":"","modifiedFiles":[]}',
+        TopicArn: 'arn:aws:sns:us-west-2:123456789123:spinnaker-github-actions'
+      }
+      mockedGetCommit.mockResolvedValueOnce({
+        data: {}
+      })
+
+      // Act
+      await run()
+
+      // Assert
+      expect(SNSClient).toBeCalledWith({ region })
+      expect(PublishCommand).toBeCalledWith(input)
+      expect(mockedSend).toBeCalledTimes(1)
+      expect(mockedGetCommit).toBeCalledWith({
+        owner: 'Org',
+        repo: 'actions-test-trigger',
+        ref: 'long-sha'
+      })
+    })
+
+    test('it returns a list of modified and added files', async () => {
+      // Arrange
+      const region = 'us-west-2'
+
+      const input = {
+        Message:
+          '{"repository":"Org/actions-test-trigger","commit":"long-sha","githubApiUrl":"https://api.github.com","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":"","modifiedFiles":["file1","file2","file4"]}',
+        TopicArn: 'arn:aws:sns:us-west-2:123456789123:spinnaker-github-actions'
+      }
+      mockedGetCommit.mockResolvedValueOnce({
+        data: {
+          files: [
+            { filename: 'file1', status: 'added' },
+            { filename: 'file2', status: 'modified' },
+            { filename: 'file3', status: 'removed' },
+            { filename: 'file4', status: 'added' }
+          ]
+        }
+      })
+
+      // Act
+      await run()
+
+      // Assert
+      expect(SNSClient).toBeCalledWith({ region })
+      expect(PublishCommand).toBeCalledWith(input)
+      expect(mockedSend).toBeCalledTimes(1)
+      expect(mockedGetCommit).toBeCalledWith({
+        owner: 'Org',
+        repo: 'actions-test-trigger',
+        ref: 'long-sha'
+      })
+    })
+
+    test('when the message is too large it returns an empty list for modifiedFiles', async () => {
+      // Arrange
+      const region = 'us-west-2'
+
+      const input = {
+        Message:
+          '{"repository":"Org/actions-test-trigger","commit":"long-sha","githubApiUrl":"https://api.github.com","ref":"main","githubEventName":"","githubActor":"","githubAction":"","parameters":{},"messageAttributes":"","modifiedFiles":[]}',
+        TopicArn: 'arn:aws:sns:us-west-2:123456789123:spinnaker-github-actions'
+      }
+      mockedGetCommit.mockResolvedValueOnce({
+        data: {
+          files: Array.from({ length: 500000 }, (_value, index) => {
+            return { filename: `file${index}`, status: 'added' }
+          })
+        }
+      })
+
+      // Act
+      await run()
+
+      // Assert
+      expect(SNSClient).toBeCalledWith({ region })
+      expect(PublishCommand).toBeCalledWith(input)
+      expect(mockedSend).toBeCalledTimes(1)
+      expect(mockedGetCommit).toBeCalledWith({
+        owner: 'Org',
+        repo: 'actions-test-trigger',
+        ref: 'long-sha'
+      })
+    })
   })
 })
 
@@ -112,5 +236,6 @@ describe('fail', () => {
     expect(SNSClient).not.toBeCalled()
     expect(PublishCommand).not.toBeCalled()
     expect(mockedSend).not.toBeCalled()
+    expect(mockedGetCommit).not.toHaveBeenCalled()
   })
 })
